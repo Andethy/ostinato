e3e974b08e617ac660e974cd5910d230773dfa65
Hamza Qadri <syedhamzaq@gmail.com>
2023-10-14 18:07:29 -0400

    Add dropdown and slider components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,65 @@
-import React, { ReactNode } from 'react';
-import logo from './logo.svg';
+import { ReactNode, useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function Tile({ children }: {children?: ReactNode}) {
   return (
-    <div className='bg-slate-200 rounded-xl w-full mb-4 p-2 shadow flex justify-between'>
+    <div className='bg-slate-200 rounded-xl w-full mb-4 p-2 shadow flex items-center justify-between'>
       {children}
     </div>
   );
 }
 
+function Dropdown({ items, onChange }: {items: string[], onChange?: (index: number) => void}) {
+  const [expanded, setExpanded] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (onChange) {
+      onChange(index);
+    }
+  }, [index]);
+
+  function onItemClick(index: number) {
+    setIndex(index);
+    setExpanded(false);
+  }
+
+  return (
+    <div className='flex flex-col'>
+      <button className='bg-white rounded p-2' onClick={() => setExpanded(!expanded)}>{items[index]}</button>
+      <div className={'flex flex-col ' + (expanded ? '' : 'hidden')}>
+        {items.map((item, index) => <button className='bg-slate-100 rounded mt-1 p-1' onClick={() => onItemClick(index)}>{item}</button>)}
+      </div>
+    </div>
+  );
+}
+
+function Slider({ min, max, step, onChange }: {min: number, max: number, step: number, onChange?: (value: number) => void}) {
+  const rangeInput = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className='-mt-2'>
+      <input ref={rangeInput} className='appearance-none w-full h-1 bg-gray-300 outline-none rounded [&::-moz-range-thumb]:bg-blue-600 [&::-moz-range-thumb]:rounded-full' type='range' min={min} max={max} step={step} onChange={() => {
+        if (onChange) {
+          onChange(rangeInput.current ? parseFloat(rangeInput.current.value) : min);
+        }
+      }} />
+    </div>
+  );
+}
+
 function App() {
+  const genres = ['Waltz'];
+  const keySigs = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+  const emotions = ['Happy', 'Sad']
+
+  const [genreIndex, setGenreIndex] = useState(0);
+  const [keySigIndex, setKeySigIndex] = useState(0);
+  const [emotionIndex, setEmotionIndex] = useState(0);
+
+  const [tempo, setTempo] = useState(60);
+  const [chaosFactor, setChaosFactor] = useState(0);
+
   return (
     <div className='App'>
       <div className='w-full p-4 shadow-md flex items-center justify-between'>
@@ -19,10 +68,23 @@ function App() {
       </div>
       <div className='flex flex-col md:flex-row w-3/5 m-auto'>
         <div className='flex-1 p-4 pr-2'>
-          <Tile>Genre</Tile>
-          <Tile>Tempo</Tile>
-          <Tile>Chaos factor</Tile>
-          <Tile>Key signature</Tile>
+          <Tile>
+            <div>Genre</div>
+            <Dropdown items={genres} onChange={index => setGenreIndex(index)} />
+          </Tile>
+          <Tile>
+            <div>Tempo</div>
+            <Slider min={60} max={180} step={1} onChange={value => setTempo(value)} />
+          </Tile>
+          <Tile>
+            <div>Chaos factor</div>
+            <Slider min={0} max={1} step={0.001} onChange={value => setChaosFactor(value)} />
+          </Tile>
+          <Tile>
+            <div>Key signature</div>
+            <Dropdown items={keySigs} onChange={index => setKeySigIndex(index)} />
+          </Tile>
+          {/*<button onClick={() => alert(genres[genreIndex] + ' ' + keySigs[keySigIndex] + ' ' + tempo + ' ' + chaosFactor)}>show params</button>*/}
         </div>
         <div className='flex-1 p-4 pl-2'>
           <Tile>Output here</Tile>
