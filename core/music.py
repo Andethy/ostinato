e5d872865f65b58656e3f82b253228658ee9bccd
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-14 14:32:06 -0400

    Add tracks for Waltz

--- core/music.py
+++ core/music.py
@@ -1,14 +1,16 @@
 from core.gpt.prompts import *
 from core.gpt.responses import Responder
+from core.instruments import HighStrings, LowStrings
 from core.midi.score import Score
 
 
 class Song:
 
-    def __init__(self, key, tempo, chaos):
+    def __init__(self, key, tempo, chaos, tracks):
         self.tempo = tempo
         self.key = key
         self.chaos = chaos
+        self.tracks = tracks
         self.score = Score()
         self.prompts = PromptManager()
         self.responder = Responder(self.prompts)
@@ -23,7 +25,7 @@ class Song:
 class Waltz(Song):
 
     def __init__(self, key, tempo, *args, **kwargs):
-        super().__init__(key, tempo, 0.5)
+        super().__init__(key, tempo, 0.5, (HighStrings(), LowStrings()))
 
     def compose_track(self, track_name):
         pass
