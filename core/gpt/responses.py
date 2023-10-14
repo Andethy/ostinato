c4b999aaf8bf57c6d93a7071f8bcb664f9a04239
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-14 18:11:35 -0400

    Add manager interface, start prompts

--- core/gpt/responses.py
+++ core/gpt/responses.py
@@ -1,11 +1,15 @@
+import os
+
 from core.gpt.prompts import *
+import openai
 
 
 class Responder:
 
-    def __init__(self, prompter: PromptManager):
+    def __init__(self, prompter: PromptManager, key):
         self.prompter = prompter
+        self.__key = os.getenv('GPT_API_KEY') if key is None else key
 
     def get_response(self, prompt_type=0):
+        self.prompter.get_prompt_by_name()
         return None
-
