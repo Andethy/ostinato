9a858bdc1d827ddfd0565a56ed29f70584625ac5
Hamza Qadri <syedhamzaq@gmail.com>
2023-10-14 14:40:49 -0400

    Add tailwind, create two column layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Tile({ children }: {children?: ReactNode}) {
+  return (
+    <div className='bg-slate-200 rounded-xl w-full mb-4 p-2 shadow flex justify-between'>
+      {children}
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='w-full p-4 shadow-md flex items-center justify-between'>
+        <h1 className='font-bold text-xl'>ostinato</h1>
+        <div/>
+      </div>
+      <div className='flex flex-col md:flex-row w-3/5 m-auto'>
+        <div className='flex-1 p-4 pr-2'>
+          <Tile>Genre</Tile>
+          <Tile>Tempo</Tile>
+          <Tile>Chaos factor</Tile>
+          <Tile>Key signature</Tile>
+        </div>
+        <div className='flex-1 p-4 pl-2'>
+          <Tile>Output here</Tile>
+        </div>
+      </div>
     </div>
   );
 }
