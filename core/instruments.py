edba17232756502a020e4a2cbeae9aa0f321e7be
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-14 16:30:28 -0400

    Update instruments

--- core/instruments.py
+++ core/instruments.py
@@ -4,19 +4,32 @@ from midi.constants import *
 class Instrument:
     inst_count = 0
 
-    def __init__(self, name, offset):
+    def __init__(self, name, offset, character):
         self.name = str(Instrument.inst_count) + '_' + name
         Instrument.inst_count += 1
         self.offset = offset
+        self.character = character
 
 
-class HighStrings(Instrument):
+class MelodicInstrument(Instrument):
+
+    def __init__(self, name, offset):
+        super().__init__(name, offset, 'melo')
+
+
+class PercussiveInstrument(Instrument):
+
+    def __init__(self, name, offset):
+        super().__init__(name, offset, 'perc')
+
+
+class HighStrings(MelodicInstrument):
 
     def __init__(self):
         super().__init__("high_strings", MID_OFFSET)
 
 
-class LowStrings(Instrument):
+class LowStrings(MelodicInstrument):
 
     def __init__(self):
         super().__init__("low_strings", BASS_OFFSET)
