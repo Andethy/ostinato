63ddd5f93ee8ad0f521e7fe5842dc9be2a46f329
Hamza Qadri <syedhamzaq@gmail.com>
2023-10-14 22:16:21 -0400

    Add audio playback

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import { ReactNode, useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function Tile({ children }: {children?: ReactNode}) {
+/*function getAudioStream(audio: HTMLAudioElement) {
+  const audCtx = new AudioContext();
+  const source = audCtx.createMediaElementSource(audio);
+  const dest = audCtx.createMediaStreamDestination();
+  source.connect(dest);
+  return dest.stream;
+}*/
+
+function Tile({ className, children }: {className?: string, children?: ReactNode}) {
   return (
-    <div className='bg-slate-200 rounded-xl w-full mb-4 p-2 shadow flex items-center justify-between'>
+    <div className={'bg-slate-200 rounded-xl w-full mb-4 p-2 shadow flex items-center justify-between ' + (className || '')}>
       {children}
     </div>
   );
@@ -62,6 +70,24 @@ function App() {
   const [tempo, setTempo] = useState(60);
   const [chaosFactor, setChaosFactor] = useState(0);
 
+  const [audio, setAudio] = useState(new Audio('https://github.com/prof3ssorSt3v3/media-sample-files/raw/master/fight-club.mp3'));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle: () => void = () => setPlaying(!playing);
+
+  useEffect(() => {
+      playing ? audio.play() : audio.pause();
+    },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
   return (
     <div className='App'>
       <div className='w-full p-4 shadow-md flex items-center justify-between'>
@@ -100,7 +126,16 @@ function App() {
           {/*<button onClick={() => alert(genres[genreIndex] + ' ' + keySigs[keySigIndex] + ' ' + tempo + ' ' + chaosFactor)}>show params</button>*/}
         </div>
         <div className='flex-1 p-4 pl-2'>
-          <Tile>Output here</Tile>
+          <Tile className='flex-col'>
+            {/*recorder && <LiveAudioVisualizer mediaRecorder={recorder} />*/}
+            <div>
+              <div/>
+              <button className='w-12 h-12 bg-pink-500 rounded-full' onClick={toggle as () => void}>
+                <FontAwesomeIcon icon={playing ? faPause : faPlay} />
+              </button>
+              <div/>
+            </div>
+          </Tile>
         </div>
       </div>
     </div>
